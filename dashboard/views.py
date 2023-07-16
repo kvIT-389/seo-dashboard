e717ea18494e8020bfa6b52b7e48c7f88ce41204
kvIT-389 <vladimirkravcov389@gmail.com>
2023-07-17 07:33:53 +0800

    Add test URLs handlers to load and get data from database

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 
 from dashboard.datahandler.metrika_handler import MetrikaHandler
 from dashboard.datahandler.topvisor_handler import TopvisorHandler
+from dashboard.datahandler.database_handler import DatabaseHandler
+
+from .models import *
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -12,9 +15,85 @@ def get_data(
     request: HttpRequest,
     data_section: str
 ) -> JsonResponse:
+    if (request.GET.get("source") == "ym"):
+        return JsonResponse(
+            MetrikaHandler().get_data(
+                data_section,
+                **request.GET
+            )
+        )
+
+    if (request.GET.get("source") == "tv"):
+        regions_indexes = TopvisorHandler().get_regions_indexes()
+        return JsonResponse(
+            dict(
+                regions_indexes=regions_indexes,
+                positions=TopvisorHandler().get_positions(
+                    regions_indexes
+                ),
+                tops=TopvisorHandler().get_tops(
+                    regions_indexes
+                )
+            )
+        )
+
+    if (data_section == "all"):
+        return JsonResponse({
+            "data": {
+                "tops": [
+                    *SearchResultsTop.objects.all().values()
+                ],
+                "positions": [
+                    *Position.objects.all().values()
+                ],
+                "visits": [
+                    *Visits.objects.all().values()
+                ],
+                "device_categories": [
+                    *DeviceCategory.objects.all().values()
+                ]
+            }
+        })
+
     return JsonResponse(
-        MetrikaHandler().get_data(
-            data_section,
-            **request.GET
+        DatabaseHandler().get_data(
+            data_section, **request.GET
         )
     )
+
+def load(
+    request: HttpRequest,
+    data_section: str
+) -> JsonResponse:
+    errors = []
+    if (data_section == "visits"):
+        errors = DatabaseHandler().load_visits(**request.GET)
+
+    if (data_section == "positions"):
+        errors = DatabaseHandler().load_positions()
+
+    if (data_section == "tops"):
+        errors = DatabaseHandler().load_tops()
+
+    return JsonResponse(dict(
+        exceptions=list(map(lambda e: str(e), errors))
+    ))
+
+def count(
+    request: HttpRequest
+) -> JsonResponse:
+    return JsonResponse({
+        "records_count": {
+            model._meta.db_table: model.objects.count()
+            for model in (
+                Visits,
+                Position,
+                TrafficSource,
+                DeviceCategory,
+                SearchEngine,
+                SearchPhrase,
+                Goal,
+                SearchResultsTop
+            )
+        }
+    })
--- server/urls.py
+++ server/urls.py
@@ -4,5 +4,7 @@ from dashboard import views
 
 urlpatterns = [
     path("", views.index),
+    path("count", views.count),
+    path("load/<str:data_section>", views.load),
     path("json/<str:data_section>", views.get_data)
 ]
