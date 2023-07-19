98952b4d74c9b2024583e1b404d5eb7d9708c60a
kvIT-389 <vladimirkravcov389@gmail.com>
2023-07-20 03:21:35 +0800

    Create trafficSources dashboard component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
-import Header from "./components/Header.vue"
+  import Header from "./components/Header.vue"
+  import TrafficSources from "./components/TrafficSources.vue"
 </script>
 
 <template>
   <Header :dashboard_name="dashboard_name"
-          @update:start-date="handleStartDateUpdated"
-          @update:end-date="handleEndDateUpdated"></Header>
+          @update:start-date="updateBeginDate"
+          @update:end-date="updateEndDate"></Header>
+
+  <TrafficSources :data="sourcesData"></TrafficSources>
 </template>
 
 <script>
@@ -17,23 +20,28 @@ import Header from "./components/Header.vue"
       return {
         dashboard_name: "Байкальский Газобетон",
         startDate: null,
-        endDate: null
+        endDate: null,
+        sourcesData: [
+          { value: 348, name: "Прямые заходы" },
+          { value: 735, name: "Переходы из поисковых систем" },
+          { value: 580, name: "Переходы по ссылкам на сайтах" },
+          { value: 484, name: "Переходы из социальных сетей" },
+          { value: 3, name: "Остальные" }
+        ]
       }
     },
     methods: {
-      handleStartDateUpdated(newStartDate) {
-        const startDate = new Date(newStartDate);
+      updateBeginDate(new_begin_date) {
+        const startDate = new Date(new_begin_date);
         const options = { day: "numeric", month: "numeric", year: "numeric" };
 
         this.startDate = startDate.toLocaleDateString("ru-RU", options);
-        this.dateone =  this.startDate;
       },
-      handleEndDateUpdated(newEndDate) {
-        const endDate = new Date(newEndDate);
+      updateEndDate(new_end_date) {
+        const endDate = new Date(new_end_date);
         const options = { day: "numeric", month: "numeric", year: "numeric" };
 
         this.endDate = endDate.toLocaleDateString("ru-RU", options);
-        this.datetwo =  this.endDate;
       }
     }
   }
---
+++ client/src/components/TrafficSources.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="chart-container">
+    <div ref="chartTrafficSources" id="chartTrafficSources"></div>
+  </div>
+</template>
+
+<script setup>
+  import { computed, onMounted, ref, defineProps } from "vue";
+  import * as echarts from "echarts";
+
+  const props = defineProps({
+    data: {
+      type: Array,
+      default: () => []
+    }
+  });
+
+  const chartOptions = computed(() => {
+    let option = {
+      title: {
+        text: "Источники трафика",
+        left: "center",
+        top: 20,
+        textStyle: {
+          
+          fontSize: 24,
+          fontWeight: "bold",
+          fontFamily: "Panton",
+          color: "#352958" 
+        }
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {}
+        }
+      },
+      tooltip: {
+        trigger: "item"
+      },
+      legend: {
+        orient: "horizontal",
+        top: "bottom",
+        textStyle: {
+          fontSize: 12,
+          fontWeight: "bold",
+          fontFamily: "Panton",
+          color: "#352958" 
+        }
+      },
+      grid: {
+        containLabel: true
+      },
+      series: [
+        {
+          name: "Access From",
+          type: "pie",
+          radius: "50%",
+          data: props.data,
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: "rgba(0, 0, 0, 0.5)"
+            }
+          }
+        }
+      ]
+    };
+
+    return option;
+  });
+
+  onMounted(() => {
+    const chart = echarts.init(document.getElementById("chartTrafficSources"));
+    chart.setOption(chartOptions.value);
+  });
+</script>
+
+<style scoped>
+  @import url("../styles/fonts.css");
+
+  .chart-title {
+    font-family: "Panton";
+  }
+  .chart-legend {
+    font-family: "Panton";
+  }
+  .chart-container {
+    width: 100%;
+    max-width: 600px;
+    margin-left: 1.5rem;
+    margin-right: auto;
+  }
+  #chartTrafficSources {
+    width: 100%;
+    height: 500px;
+    background-color: white;
+    border-radius: 10px;
+  }
+</style>
