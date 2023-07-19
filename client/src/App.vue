dd77bcf5c3dfb8dedffc8232c9e1524b1a2e3033
kvIT-389 <vladimirkravcov389@gmail.com>
2023-07-20 02:39:58 +0800

    Create Header component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,40 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue"
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Header :dashboard_name="dashboard_name"
+          @update:start-date="handleStartDateUpdated"
+          @update:end-date="handleEndDateUpdated"></Header>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+  export default {
+    components: {
+      Header
+    },
+    data() {
+      return {
+        dashboard_name: "Байкальский Газобетон",
+        startDate: null,
+        endDate: null
+      }
+    },
+    methods: {
+      handleStartDateUpdated(newStartDate) {
+        const startDate = new Date(newStartDate);
+        const options = { day: "numeric", month: "numeric", year: "numeric" };
+
+        this.startDate = startDate.toLocaleDateString("ru-RU", options);
+        this.dateone =  this.startDate;
+      },
+      handleEndDateUpdated(newEndDate) {
+        const endDate = new Date(newEndDate);
+        const options = { day: "numeric", month: "numeric", year: "numeric" };
+
+        this.endDate = endDate.toLocaleDateString("ru-RU", options);
+        this.datetwo =  this.endDate;
+      }
+    }
+  }
+</script>
